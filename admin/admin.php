a6066196f5f48d6f056438a5086ec2994cfee532
tino23001 <tino23001@student.hamk.fi>
2024-02-05 16:23:29 +0200

    admin testausta 2

--- admin/admin.php
+++ admin/admin.php
@@ -2,6 +2,7 @@
 <html lang="fi">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
     <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz@8..144&family=Snippet&family=Source+Sans+3&display=swap" rel="stylesheet">
@@ -19,5 +20,14 @@
     ?>
 
 <h1>Admin-sivu</h1>
+
+    <div class="container">
+        <div class="box">
+            <p>Tervetuloa <?php echo $_SESSION['kayttaja']; ?>!</p>
+            <a href="Kauppaedit.php">Kauppasivun muokkaustila</a><br><br>
+            <a href="logout.php">Kirjaudu ulos</a>
+        </div>
+    </div>
+
 </body>
 </html>
