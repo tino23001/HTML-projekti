6d05de479c2bee4f8a3f4f367e38969fac1ba00f
Maiqqe <maisa.tuomenpuro@student.hamk.fi>
2024-02-06 21:47:01 +0200

    Vian määritys Poista_tuote
    Poista_tuote kunnossa

--- admin/poista_tuote.php
+++ admin/poista_tuote.php
@@ -9,26 +9,43 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
+
 <?php
-// Sisällytä tietokantayhteyden määrittelyt
 include('config.php');
 
 // Otetaan vastaan tuote_id, jonka perusteella poistetaan tuote tietokannasta
 if(isset($_POST['tuote_id'])) {
+    try {
+        // Poistetaan tuote tietokannasta
+        $tuote_id = $_POST['tuote_id'];
+        $sql = "DELETE FROM tuotteet WHERE tuote_id = :tuote_id";
+        $statement = $pdo->prepare($sql);
+        $statement->execute([':tuote_id' => $tuote_id]);
 
-    // Poistetaan tuote tietokannasta
-    $tuote_id = $_POST['tuote_id'];
-    $sql = "DELETE FROM tuotteet WHERE tuote_id = :tuote_id";
-    $statement = $pdo->prepare($sql);
-    $statement->execute([':tuote_id' => $tuote_id]);
+        // Palautetaan vastaus, joka kertoo, että poisto onnistui
+        echo "success";
 
-    echo "Tuote poistettu onnistuneesti";
+        // Lisää lokituloste onnistuneesta poistosta
+        error_log("Tuote poistettu onnistuneesti. Tuote ID: $tuote_id");
+
+    } catch (PDOException $e) {
+        // Virheenkäsittely: Palautetaan virheilmoitus, jos poistaminen epäonnistui
+        echo "Virhe: " . $e->getMessage();
+
+        // Lisää lokituloste virheestä
+        error_log("Virhe poistettaessa tuotetta. Virhe: " . $e->getMessage());
+    }
 } else {
+    // Palautetaan virheilmoitus, jos tuote_id:tä ei ole määritetty
     echo "Virhe: Tuote_id ei ole määritetty";
+
+    // Lisää lokituloste tuote_id:n puuttumisesta
+    error_log("Virhe poistettaessa tuotetta: Tuote_id ei ole määritetty");
 }
 ?>
 
-?>
+
+
 </body>
 </html>
 
