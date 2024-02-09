e386c3278ab0e57a1ddf735a3371993dc329680f
Henna <hennariina23000@student.hamk.fi>
2024-02-09 20:45:40 +0200

    lisaatuote.php muokkausta

--- admin/lisaatuote.php
+++ admin/lisaatuote.php
@@ -1,3 +1,75 @@
+<?php
+session_start();
+include('config.php');
+
+$message = ''; // Alustetaan viesti tyhjänä
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $productName = htmlspecialchars($_POST['product_name']);
+    $productPrice = filter_var($_POST['product_price'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $productDescription = htmlspecialchars($_POST['product_description']);
+
+    // Tarkistetaan tuotenimen olemassaolo
+    $stmt = $pdo->prepare("SELECT COUNT(*) FROM tuotteet WHERE nimi = ?");
+    $stmt->execute([$productName]);
+    $count = $stmt->fetchColumn();
+
+    if ($count > 0) {
+        $_SESSION['message'] = "Tuote nimellä '{$productName}' on jo olemassa. Valitse toinen nimi.";
+        header('Location: ' . $_SERVER['PHP_SELF']);
+        exit();
+    } elseif (!preg_match('/^[a-zA-Z0-9]+$/', $productName) || strlen($productName) > 30) {
+        $_SESSION['message'] = "Virhe! Tarkista tuotteen nimi. Nimen pituus ei saa ylittää 30 merkkiä ja sen tulee sisältää vain kirjaimia ja numeroita.";
+        header('Location: ' . $_SERVER['PHP_SELF']);
+        exit();
+    } elseif ($productPrice <= 0 || !is_numeric($productPrice)) {
+        $_SESSION['message'] = "Virhe! Hinnan tulee olla positiivinen numero.";
+        header('Location: ' . $_SERVER['PHP_SELF']);
+        exit();
+    }
+    
+    // Tiedoston MIME-tyypin tarkistus
+    $fileMimeType = mime_content_type($_FILES["product_image"]["tmp_name"]);
+    if (!in_array($fileMimeType, ['image/jpeg', 'image/png', 'image/gif'])) {
+        $_SESSION['message'] = "Vain JPG, JPEG, PNG & GIF -tiedostot ovat sallittuja.";
+        header('Location: ' . $_SERVER['PHP_SELF']);
+        exit();
+    }
+    
+    // Tiedoston latauslogiikka
+    $targetDir = "../uploads";
+    $uniqueImageName = uniqid() . basename($_FILES["product_image"]["name"]);
+    $targetFile = $targetDir . DIRECTORY_SEPARATOR . $uniqueImageName;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $allowedFileTypes = ['jpg', 'jpeg', 'png', 'gif'];
+    $maxFileSize = 5000000; // 5MB
+    $imageSize = $_FILES["product_image"]["size"];
+
+        if (!in_array($imageFileType, $allowedFileTypes) || $imageSize > $maxFileSize) {
+            $_SESSION['message'] = "Vain JPG, JPEG, PNG & GIF -tiedostot ovat sallittuja ja kuvan maksimikoko on 5MB.";
+            header('Location: ' . $_SERVER['PHP_SELF']);
+            exit();
+        } elseif (move_uploaded_file($_FILES["product_image"]["tmp_name"], $targetFile)) {
+            try {
+                $sql = "INSERT INTO tuotteet (nimi, hinta, kuva, kuvaus) VALUES (?, ?, ?, ?)";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute([$productName, $productPrice, $uniqueImageName, $productDescription]);
+                $_SESSION['message'] = "Tuote lisätty onnistuneesti.";
+                header('Location: ' . $_SERVER['PHP_SELF']);
+                exit();
+            } catch (PDOException $e) {
+                $_SESSION['message'] = "Tietokantavirhe: " . $e->getMessage();
+                header('Location: ' . $_SERVER['PHP_SELF']);
+                exit();
+            }
+        } else {
+            $_SESSION['message'] = "Virhe ladattaessa kuvaa.";
+            header('Location: ' . $_SERVER['PHP_SELF']);
+            exit();
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="fi">
 <head>
@@ -16,57 +88,17 @@
     </header>
 
     <div class="contact-container">
+
         <section class="contact-form">
-            <?php
-            $message = ''; // Alustetaan viesti tyhjänä
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                include('config.php');
-                $productName = htmlspecialchars($_POST['product_name']);
-                $productPrice = filter_var($_POST['product_price'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-                $productDescription = htmlspecialchars($_POST['product_description']);
-
-                // Tarkistetaan tuotenimen olemassaolo
-                $stmt = $pdo->prepare("SELECT COUNT(*) FROM tuotteet WHERE nimi = ?");
-                $stmt->execute([$productName]);
-                $count = $stmt->fetchColumn();
-
-                if ($count > 0) {
-                    $message = "Tuote nimellä '{$productName}' on jo olemassa. Valitse toinen nimi.";
-                } elseif (!preg_match('/[a-zA-Z0-9]/', $productName) || strlen($productName) > 30) {
-                    $message = "Virhe! Tarkista tuotteen nimi. Nimen pituus ei saa ylittää 30 merkkiä ja sen tulee sisältää vain kirjaimia ja numeroita.";
-                } elseif ($productPrice <= 0 || !is_numeric($productPrice)) {
-                    $message = "Virhe! Hinnan tulee olla positiivinen numero.";
-                } else {
-                    // Kuvan käsittelyn ehdot
-                    $targetDir = "../uploads"; 
-                    $uniqueImageName = uniqid() . basename($_FILES["product_image"]["name"]);
-                    $targetFile = $targetDir . DIRECTORY_SEPARATOR . $uniqueImageName;
-                    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-                    $allowedFileTypes = ['jpg', 'jpeg', 'png', 'gif'];
-                    $maxFileSize = 5000000; // 5MB
-                    $imageSize = $_FILES["product_image"]["size"];
-
-                    if (!in_array($imageFileType, $allowedFileTypes) || $imageSize > $maxFileSize) {
-                        $message = "Vain JPG, JPEG, PNG & GIF -tiedostot ovat sallittuja ja kuvan maksimikoko on 5MB.";
-                    } elseif (move_uploaded_file($_FILES["product_image"]["tmp_name"], $targetFile)) {
-                        try {
-                            $sql = "INSERT INTO tuotteet (nimi, hinta, kuva, kuvaus) VALUES (?, ?, ?, ?)";
-                            $stmt = $pdo->prepare($sql);
-                            $stmt->execute([$productName, $productPrice, $uniqueImageName, $productDescription]);
-                            $message = "Tuote lisätty onnistuneesti.";
-                        } catch (PDOException $e) {
-                            $message = "Tietokantavirhe: " . $e->getMessage();
-                        }
-                    } else {
-                        $message = "Virhe ladattaessa kuvaa.";
-                    }
-                }
-            }
-            if (!empty($message)) {
-                echo "<p>$message</p>";
-            }
-            ?>
-
+        <?php
+        // Tarkistetaan, onko istunnossa viestiä ja näytetään se
+        if (isset($_SESSION['message'])) {
+            $message = $_SESSION['message'];
+            unset($_SESSION['message']);
+            // Tämä osio näyttää viestin HTML-sivulla
+            echo "<div class='message'>$message</div>";
+        }
+        ?>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
                 <h1>Lisää tuote</h1>
                 <input type="text" name="product_name" placeholder="Tuotteen nimi" required>
@@ -76,61 +108,10 @@
                 <button type="submit">Tallenna tuote</button>
             </form>
         </section>
-    </div>
-    <footer>
-        <div class="footer-container">
-            <div class="footer-section">
-                <h4>Asiakaspalvelu</h4>
-               
-                <p><strong>Puhelin:</strong> <a href="tel:+358201234567">+358 20 123 4567</a></p>
-                <a href="Yhteystiedot.html">Yhteydenottolomake</a>
-            </div>
-            <div class="footer-section">
-                <h4>Info</h4>
-                <a href="#palautukset">Palautukset</a>
-                <a href="#takuu">Takuu</a>
-                <a href="#maksutavat">Maksutavat</a>
-                <a href="#toimitustavat">Toimitustavat</a>
-            </div>
-            <div class="footer-section">
-                <h4>Resurssit</h4>
-                <a href="#vinkit-ja-osto-oppaat">Vinkit ja osto-oppaat</a>
-                <a href="#vari-ja-luonneanalyysi">Väri- ja luonneanalyysi</a>
-                <a href="#koulutusopas">Koulutusopas</a>
-            </div>
-            <div class="footer-section">
-                <h4>Palvelut</h4>
-                <a href="#yritysmyynti">Yritysmyynti</a>
-                <a href="#neuvontapalvelut">Neuvontapalvelut</a>
-                <a href="#elainlaakaripalvelut">Eläinlääkäripalvelut</a>
-            </div>
-            <div class="footer-section">
-              <h4>Sosiaalinen media</h4>
-              <div class="social-media-links">
-                  <a href="https://www.facebook.com" target="_blank">
-                      <img src="fb.png" alt="Facebook" class="social-icon">
-                      <span>Facebook</span>
-                  </a>
-                  <a href="https://www.instagram.com/yourpage" target="_blank">
-                      <img src="ig.png" alt="Instagram" class="social-icon">
-                      <span>Instagram</span>
-                  </a>
-                  <a href="https://www.linkedin.com/yourpage" target="_blank">
-                      <img src="linkedin.png" alt="LinkedIn" class="social-icon">
-                      <span>LinkedIn</span>
-                  </a>
-                  <a href="https://www.youtube.com/yourpage" target="_blank">
-                      <img src="youtube.png" alt="YouTube" class="social-icon">
-                      <span>YouTube</span>
-                  </a>
-              </div>
-          </div>
         </div>
-        <div class="footer-bottom">
-            © Lohikärmes 2024
-        </div>
-    </footer>
-
+            <footer class="footer-bottom-admin">
+                © Lohikärmes 2024
+            </footer>
+            </div>
 </body>
 </html>
-
