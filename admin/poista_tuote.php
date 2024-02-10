0336187c345b7cad25b161422c2b41dc3529ce4c
Henna <hennariina23000@student.hamk.fi>
2024-02-10 21:06:23 +0200

    poista tuote -ominaisuuden muokkausta

--- admin/poista_tuote.php
+++ admin/poista_tuote.php
@@ -4,15 +4,19 @@ include('config.php');
 
 // Otetaan vastaan tuote_id, jonka perusteella poistetaan tuote tietokannasta
 if(isset($_POST['tuote_id'])) {
-        // Poistetaan tuote tietokannasta
-        $tuote_id = $_POST['tuote_id'];
-        $sql = "DELETE FROM tuotteet WHERE tuote_id = :tuote_id";
-        $sql = "DELETE FROM arvostelut WHERE tuote_id = :tuote_id";
-        $statement = $pdo->prepare($sql);
-        $statement->execute([':tuote_id' => $tuote_id]);
+    $tuote_id = $_POST['tuote_id'];
 
-        echo "success";
+    // Ensin poistetaan tuotteeseen liittyvät arvostelut
+    $sql_arvostelut = "DELETE FROM arvostelut WHERE tuote_id = :tuote_id";
+    $stmt_arvostelut = $pdo->prepare($sql_arvostelut);
+    $stmt_arvostelut->execute([':tuote_id' => $tuote_id]);
 
+    // Sitten poistetaan itse tuote
+    $sql_tuote = "DELETE FROM tuotteet WHERE tuote_id = :tuote_id";
+    $stmt_tuote = $pdo->prepare($sql_tuote);
+    $stmt_tuote->execute([':tuote_id' => $tuote_id]);
+
+    echo "success";
 } else {
     echo "Virhe: Tuote_id ei ole määritetty";
 }
